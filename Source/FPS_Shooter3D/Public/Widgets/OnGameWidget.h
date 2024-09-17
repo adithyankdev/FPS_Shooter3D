cc8ebc33c40da5b8daa2358f7202301aab71b025
Adithyan K <adx00k@gmail.com>
2024-09-17 14:16:27 +0530

    Gun CrossHair Added

---
+++ Source/FPS_Shooter3D/Public/Widgets/OnGameWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "OnGameWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_SHOOTER3D_API UOnGameWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
